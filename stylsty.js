83d0538690dcfab58e34dfa2413fe09cabecc2a2
clara_nevess <135254488+claraneves@users.noreply.github.com>
2023-10-27 13:12:49 -0300

    .

---
+++ stylsty.js
@@ -0,0 +1,9 @@
+function abrirNav() {
+    document.getElementById("menuoculto").style.width = "250px";
+    document.getElementById("principal").style.marginLeft = "250px";
+  }
+  function fecharNav() {
+    document.getElementById("menuoculto").style.width = "0";
+    document.getElementById("principal").style.marginLeft = "0";
+  }
+  
